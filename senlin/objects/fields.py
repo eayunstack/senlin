261d874e956ce7452c6ce86379a6cf3dad553ee9
tengqm <tengqim@cn.ibm.com>
2016-10-20 05:25:52 -0400

    Add 'Boolean' and 'AdjustmentType' fields
    The oslo version of 'Boolean' field (type) is virtually not usuable.
    We propose our own reason for validation's purpose. The AdjustmentType
    field type is a lot easier to understand.
    
    Change-Id: I4a140c290124899bc36cd0628cc3944a7f9bc345
    (cherry picked from commit 3df1e20bed6f36b543e1b7c9f49f1831a876b548)

--- senlin/objects/fields.py
+++ senlin/objects/fields.py
@@ -11,18 +11,111 @@
 # under the License.
 
 from oslo_serialization import jsonutils
+from oslo_utils import strutils
 from oslo_versionedobjects import fields
 import six
 
+from senlin.common.i18n import _
+
 # Field alias for code readability
-BooleanField = fields.BooleanField
+FlexibleBooleanField = fields.FlexibleBooleanField
 StringField = fields.StringField
 IntegerField = fields.IntegerField
 FloatField = fields.FloatField
 UUIDField = fields.UUIDField
 DateTimeField = fields.DateTimeField
 ListOfStringsField = fields.ListOfStringsField
-ObjectField = fields.ObjectField
+ListOfEnumField = fields.ListOfEnumField
+
+
+class Boolean(fields.FieldType):
+    # NOTE: The following definition is much more stricter than the oslo
+    #       version. Also note that the treatment of default values here:
+    #       we are using the user specified default value when invoking
+    #       the 'bool_from_string' until function.
+
+    def __init__(self, default=False):
+        super(Boolean, self).__init__()
+        self._default = default
+
+    def coerce(self, obj, attr, value):
+        return strutils.bool_from_string(value, strict=True,
+                                         default=self._default)
+
+    def get_schema(self):
+        return {'type': ['boolean']}
+
+
+# TODO(Qiming): remove this when oslo patch is released
+# https://review.openstack.org/#/c/360095
+class NonNegativeInteger(fields.FieldType):
+
+    @staticmethod
+    def coerce(obj, attr, value):
+        v = int(value)
+        if v < 0:
+            err = _("Value must be >= 0 for field '%s'.") % attr
+            raise ValueError(err)
+        return v
+
+    def get_schema(self):
+        return {
+            'type': ['integer', 'string'],
+            'minimum': 0
+        }
+
+
+# TODO(Qiming): remove this when oslo patch is released
+# i.e. global requirements bump to 1.17.0
+class Object(fields.Object):
+
+    def get_schema(self):
+        from oslo_versionedobjects import base as obj_base
+        obj_classes = obj_base.VersionedObjectRegistry.obj_classes()
+        if self._obj_name in obj_classes:
+            cls = obj_classes[self._obj_name][0]
+            namespace_key = cls._obj_primitive_key('namespace')
+            name_key = cls._obj_primitive_key('name')
+            version_key = cls._obj_primitive_key('version')
+            data_key = cls._obj_primitive_key('data')
+            changes_key = cls._obj_primitive_key('changes')
+            field_schemas = {key: field.get_schema()
+                             for key, field in cls.fields.items()}
+            required_fields = [key for key, field in sorted(cls.fields.items())
+                               if not field.nullable]
+
+            schema = {
+                'type': 'object',
+                'properties': {
+                    namespace_key: {
+                        'type': 'string',
+                    },
+                    name_key: {
+                        'type': 'string',
+                    },
+                    version_key: {
+                        'type': 'string',
+                    },
+                    changes_key: {
+                        'type': 'array',
+                        'items': {
+                            'type': 'string',
+                        }
+                    },
+                    data_key: {
+                        'type': 'object',
+                        'description': 'fields of %s' % self._obj_name,
+                        'properties': field_schemas,
+                        'required': required_fields,
+                        'additionalProperties': False,
+                    },
+                },
+                'required': [namespace_key, name_key, version_key, data_key],
+            }
+
+            return schema
+        else:
+            return {}
 
 
 class Json(fields.FieldType):
@@ -50,14 +143,6 @@ class Json(fields.FieldType):
 
     def get_schema(self):
         return {'type': ['object']}
-
-
-class JsonField(fields.AutoTypedField):
-    AUTO_TYPE = Json()
-
-
-class ListField(fields.AutoTypedField):
-    AUTO_TYPE = fields.List(fields.FieldType())
 
 
 class NotificationPriority(fields.Enum):
@@ -99,6 +184,216 @@ class NotificationAction(fields.Enum):
             valid_values=NotificationAction.ALL)
 
 
+class Name(fields.String):
+
+    def __init__(self, min_len=1, max_len=255):
+        super(Name, self).__init__()
+        self.min_len = min_len
+        self.max_len = max_len
+
+    def coerce(self, obj, attr, value):
+        err = None
+        if len(value) < self.min_len:
+            err = _("The value for the %(attr)s field must be at least "
+                    "%(count)d characters long."
+                    ) % {'attr': attr, 'count': self.min_len}
+        elif len(value) > self.max_len:
+            err = _("The value for the %(attr)s field must be less than "
+                    "%(count)d characters long."
+                    ) % {'attr': attr, 'count': self.max_len}
+        else:
+            # NOTE: This is pretty restrictive. We can relax it later when
+            # there are requests to do so
+            regex = re.compile('^[a-zA-Z\d\.\_\~-]*$', re.IGNORECASE)
+            if not regex.search(value):
+                err = _("The value for the '%(attr)s' (%(value)s) contains "
+                        "illegal characters."
+                        ) % {'attr': attr, 'value': value}
+
+        if err:
+            raise ValueError(err)
+
+        return super(Name, self).coerce(obj, attr, value)
+
+    def get_schema(self):
+        return {
+            'type': ['string'],
+            'minLength': self.min_len,
+            'maxLength': self.max_len
+        }
+
+
+class Capacity(fields.Integer):
+
+    def __init__(self, minimum=0, maximum=None):
+        super(Capacity, self).__init__()
+        CONF.import_opt("max_nodes_per_cluster", "senlin.common.config")
+
+        if minimum > CONF.max_nodes_per_cluster:
+            err = _("The value of 'minimum' cannot be greater than the global "
+                    "constraint (%(m)d).") % {'m': CONF.max_nodes_per_cluster}
+            raise ValueError(err)
+        self.minimum = minimum
+
+        if maximum is not None:
+            if maximum < minimum:
+                err = _("The value of 'maximum' must be greater than or equal "
+                        "to that of the 'minimum' specified.")
+                raise ValueError(err)
+
+            if maximum > CONF.max_nodes_per_cluster:
+                err = _("The value of 'maximum' cannot be greater than the "
+                        "global constraint (%(m)d)."
+                        ) % {'m': CONF.max_nodes_per_cluster}
+                raise ValueError(err)
+
+            self.maximum = maximum
+        else:
+            self.maximum = CONF.max_nodes_per_cluster
+
+    def coerce(self, obj, attr, value):
+        value = super(Capacity, self).coerce(obj, attr, value)
+
+        err = None
+        if value < self.minimum:
+            err = _("The value for the %(a)s field must be greater than or "
+                    "equal to %(n)d.") % {'a': attr, 'n': self.minimum}
+        elif value > self.maximum:
+            err = _("The value for the %(a)s field must be less than or equal "
+                    "to %(n)d.") % {'a': attr, 'n': self.maximum}
+        if err:
+            raise ValueError(err)
+
+        return value
+
+    def get_schema(self):
+        return {
+            'type': ['integer', 'string'],
+            'minimum': self.minimum,
+            'maximum': self.maximum,
+            'pattern': '^[0-9]*$',
+        }
+
+
+class Sort(fields.String):
+
+    def __init__(self, valid_keys):
+        super(Sort, self).__init__()
+        self.valid_keys = valid_keys
+
+    def coerce(self, obj, attr, value):
+        for s in value.split(','):
+            s_key, _sep, s_dir = s.partition(':')
+            err = None
+            if not s_key:
+                err = _("missing sort key for '%s'.") % attr
+                raise ValueError(err)
+
+            if s_key not in self.valid_keys:
+                err = _("unsupported sort key '%(value)s' for '%(attr)s'."
+                        ) % {'attr': attr, 'value': s_key}
+
+            if s_dir and s_dir not in ('asc', 'desc'):
+                err = _("unsupported sort dir '%(value)s' for '%(attr)s'."
+                        ) % {'attr': attr, 'value': s_dir}
+
+            if err:
+                raise ValueError(err)
+
+        return super(Sort, self).coerce(obj, attr, value)
+
+    def get_schema(self):
+        return {
+            'type': ['string'],
+        }
+
+
+class IdentityList(fields.List):
+
+    def __init__(self, element_type, min_items=0, unique=True, nullable=False,
+                 **kwargs):
+        super(IdentityList, self).__init__(element_type, **kwargs)
+        self.min_items = min_items
+        self.unique_items = unique
+        self.nullable = nullable
+
+    def coerce(self, obj, attr, value):
+        res = super(IdentityList, self).coerce(obj, attr, value)
+        if len(res) < self.min_items:
+            raise ValueError(_("Value for '%(attr)s' must have at least "
+                               "%(num)s item(s).") %
+                             {'attr': attr, 'num': self.min_items})
+        if len(set(res)) != len(res) and self.unique_items:
+            raise ValueError(_("Items for '%(attr)s' must be unique") %
+                             {'attr': attr})
+        return res
+
+    def get_schema(self):
+        schema = super(IdentityList, self).get_schema()
+        if self.nullable:
+            schema['type'].append('null')
+        schema['minItems'] = self.min_items
+        schema['uniqueItems'] = self.unique_items
+        return schema
+
+
+class BaseEnum(fields.Enum):
+
+    def __init__(self, **kwargs):
+        super(BaseEnum, self).__init__(valid_values=self.__class__.ALL)
+
+    def get_schema(self):
+        # TODO(Anyone): remove this override when the following patch is
+        # merged and released.
+        # https://review.openstack.org/389061
+        schema = {
+            'type': ['string'],
+            'enum': self._valid_values
+        }
+        return schema
+
+
+class AdjustmentType(BaseEnum):
+
+    ALL = (
+        EXACT_CAPACITY, CHANGE_IN_CAPACITY, CHANGE_IN_PERCENTAGE,
+    ) = (
+        'EXACT_CAPACITY', 'CHANGE_IN_CAPACITY', 'CHANGE_IN_PERCENTAGE',
+    )
+
+
+class BooleanField(fields.AutoTypedField):
+
+    AUTO_TYPE = None
+
+    def __init__(self, default=False, **kwargs):
+        self.AUTO_TYPE = Boolean(default=default)
+        super(BooleanField, self).__init__(**kwargs)
+
+
+# TODO(Qiming): remove this when oslo patch is released
+# https://review.openstack.org/#/c/360095
+class NonNegativeIntegerField(fields.AutoTypedField):
+
+    AUTO_TYPE = NonNegativeInteger()
+
+
+class ObjectField(fields.AutoTypedField):
+
+    def __init__(self, objtype, subclasses=False, **kwargs):
+        self.AUTO_TYPE = Object(objtype, subclasses)
+        self.objname = objtype
+        super(ObjectField, self).__init__(**kwargs)
+
+
+class JsonField(fields.AutoTypedField):
+    AUTO_TYPE = Json()
+
+
+class ListField(fields.AutoTypedField):
+    AUTO_TYPE = fields.List(fields.FieldType())
+
+
 class NotificationPriorityField(fields.BaseEnumField):
     AUTO_TYPE = NotificationPriority()
 
@@ -109,3 +404,44 @@ class NotificationPhaseField(fields.BaseEnumField):
 
 class NotificationActionField(fields.BaseEnumField):
     AUTO_TYPE = NotificationAction()
+
+
+class NameField(fields.AutoTypedField):
+
+    AUTO_TYPE = Name()
+
+
+class CapacityField(fields.AutoTypedField):
+
+    AUTO_TYPE = None
+
+    def __init__(self, nullable=False, default=None, minimum=0, maximum=None):
+        self.AUTO_TYPE = Capacity(minimum=minimum, maximum=maximum)
+        super(CapacityField, self).__init__(nullable=nullable, default=default)
+
+
+class SortField(fields.AutoTypedField):
+
+    AUTO_TYPE = None
+
+    def __init__(self, valid_keys, nullable=False, default=None):
+        self.AUTO_TYPE = Sort(valid_keys)
+        super(SortField, self).__init__(nullable=nullable, default=default)
+
+
+class IdentityListField(fields.AutoTypedField):
+
+    AUTO_TYPE = None
+
+    def __init__(self, min_items=0, unique=True, nullable=False, default=None):
+        if default is None:
+            default = []
+        self.AUTO_TYPE = IdentityList(fields.String(), min_items=min_items,
+                                      unique=unique)
+        super(IdentityListField, self).__init__(nullable=nullable,
+                                                default=default)
+
+
+class AdjustmentTypeField(fields.BaseEnumField):
+
+    AUTO_TYPE = AdjustmentType()
